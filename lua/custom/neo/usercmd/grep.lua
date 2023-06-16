d76720da4098eb34bf847a402b52afcf90368ba2
Martin Braun <martin.braun.1991@live.com>
2023-06-16 19:03:44 +0200

    fix: spaces in Grep user command regex cause arg confusion

--- lua/custom/neo/usercmd/grep.lua
+++ lua/custom/neo/usercmd/grep.lua
@@ -1,42 +1,63 @@
 -- Grep
 vim.api.nvim_create_user_command(
-  'Grep',
-  function(opts)
-    if #opts.args < 1 then
-      print("Usage: :Grep(!) <pattern> <filter?>, i.e. :Grep /my_string/g **/*, or :Grep! /my_string/my_replacement/g **/*")
-      return
+'Grep',
+function(opts)
+    local usage = "Usage: :Grep <regex> <filter?>, i.e. :Grep /my_string/g **/*, or :Grep! s/my_string/my_replacement/g **/*"
+    if not opts.args or string.len(opts.args) < 1 then
+        print(usage)
+        return
     end
-    local pattern = opts.fargs[1]
-    local filter = opts.fargs[2] or "**/*"
-    local segmno = 1
-    local searchpatt = nil
+    local delim = string.sub(opts.args, 1, 1)
+    if delim == "s" then
+        delim = string.sub(opts.args, 2, 2)
+    end
+    local pargs = vim.split(opts.args, delim)
+    if #pargs < 3 then
+        print(usage)
+        return
+    end
+    local searchpatt = pargs[2]
+    if pargs[1] == "s" and #pargs < 4 then
+        print(usage)
+        return
+    end
     local replpatt = nil
     local flags = nil
-    for pattsegm in string.gmatch(pattern, "([^/]+)") do
-      if segmno == 1 then
-        searchpatt = pattsegm
-      elseif segmno == 2 then
+    local filter = ""
+    if pargs[1] == "s" then
         if not opts.bang then
-          flags = pattsegm
-        else
-          replpatt = pattsegm
+            print(usage)
+            return
         end
-      elseif segmno == 3 then
-        flags = pattsegm
-      else
-        break
-      end
-      segmno = segmno + 1
+        replpatt = pargs[3]
+        local bridge = vim.split(pargs[4], " ")
+        flags = bridge[1]
+        filter = bridge[2] or ""
+        for i = 5, #pargs do
+            filter = filter .. delim.. pargs[i]
+        end
+    else
+        if opts.bang then
+            print(usage)
+            return
+        end
+        local bridge = vim.split(pargs[3], " ")
+        flags = bridge[1]
+        filter = bridge[2] or ""
+        for i = 4, #pargs do
+            filter = filter .. delim .. pargs[i]
+        end
     end
+    -- print("d:" .. delim .. "\ns:" .. searchpatt .. "\nr:" .. (replpatt or "nil") .. "\nm:" .. flags .. "\nf:" .. filter)
     if searchpatt and flags and filter then
-      vim.cmd(":vimgrep /" .. searchpatt .. "/" .. flags .. "j " .. filter)
-      vim.cmd(":cw")
-      vim.cmd(":.cc")
-      if opts.bang and replpatt then
-        vim.cmd(":cfdo %s/" .. searchpatt .. "/" .. replpatt .. "/" .. flags .. "e")
-      end
+        vim.cmd(":vimgrep /" .. searchpatt .. "/" .. flags .. "j " .. filter)
+        vim.cmd(":cw")
+        vim.cmd(":.cc")
+        if  replpatt then
+            vim.cmd(":cfdo %s/" .. searchpatt .. "/" .. replpatt .. "/" .. flags .. "e")
+        end
     end
-  end,
-  { nargs = '*', bang = true }
+end,
+{ nargs = '*', bang = true }
 )
 
